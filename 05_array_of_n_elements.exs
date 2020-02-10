82bccef4cc02bf15ef909a2d8eb685da5f36fa8f
Meyhem <meyhem@protonmail.com>
2020-02-10 20:57:21 +0100

    array of n elements

---
+++ 05_array_of_n_elements.exs
@@ -0,0 +1,18 @@
+defmodule Solution do
+  def read_int() do
+    case IO.gets "" do
+      line when is_binary(line) -> { :ok, String.to_integer String.trim line }
+      :eof -> { :error }
+      { :error, _} -> { :error }
+    end
+  end
+
+  def main() do
+    { _, n} = read_int()
+
+    IO.puts "[" <> Enum.reduce(1..n, fn n, acc -> "#{acc}, #{n}" end) <> "]"
+  end
+
+end
+
+Solution.main
