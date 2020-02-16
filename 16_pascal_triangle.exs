27b801ebba033b624eba95fc6b4da27481678908
Meyhem <meyhem@protonmail.com>
2020-02-16 14:59:06 +0100

    fIbonacci, pascal

--- 16_pascal_triangle.exs
+++ 16_pascal_triangle.exs
@@ -1,4 +1,4 @@
-# echo -e "3" | elixir 15_fibonacci.exs
+# echo -e "5" | elixir 16_pascal_triangle.exs
 defmodule Solution do
   def read_float() do
     case IO.gets "" do
@@ -27,15 +27,21 @@ defmodule Solution do
     end
   end
 
-  def fib(n) when n > 2, do: fib(n - 1) + fib(n - 2)
-  def fib(2), do: 1
-  def fib(1), do: 0
+  def fac(0), do: 1
+  def fac(n) when n > 0, do: n * fac(n - 1)
+
+  def pascal(row, col) do
+    fac(row) / (fac(col) * fac(row - col))
+  end
 
   def main() do
     { _, n} = read_int()
 
-    fib(n)
-    |> IO.inspect
+    0..n-1
+    |> Enum.map(fn r -> Enum.map(0..r, fn c -> pascal(r, c) end) end)
+    |> Enum.map(fn e -> Enum.join(Enum.map(e, &trunc/1), " ") end)
+    |> Enum.map(&IO.puts/1)
+
   end
 
 end
